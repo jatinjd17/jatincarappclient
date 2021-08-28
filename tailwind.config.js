454ca403240d7b5cc1c33cfbeba91091b0d29579
jatinjd17 <jatindharmik@gmail.com>
2021-08-28 14:05:45 +0530

    ignore111

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,11 @@
 module.exports = {
-  purge: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-      "2xl": { max: "2000px" },
-      // => @media (max-width: 1535px) { ... }
-
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-
-      md: { max: "897px" },
-      // => @media (max-width: 767px) { ... }
-
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
-    },
     extend: {},
   },
   variants: {
-    extend: { translate: ["motion-reduce"] },
+    extend: {},
   },
   plugins: [],
-};
+}
