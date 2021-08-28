fbc47a70ad5323a1b8134b088f749a67c8e2da57
jatinjd17 <jatindharmik@gmail.com>
2021-08-28 15:42:12 +0530

    amaze

--- pages/honda/amaze/index.tsx
+++ pages/honda/amaze/index.tsx
@@ -469,11 +469,102 @@ function Homee({ joy }) {
         </div>
       </div>
 
-      {typeof window !== "undefined" && (
+      {/* {typeof window !== "undefined" && (
         <DynamicSimilarCar SimilarCars={SimilarCars} />
-      )}
+      )} */}
 
       {/* <SimilarCarss SimilarCars={SimilarCars} /> */}
+
+      <div className="mx-64 md:mx-2">
+        <Carousel responsive={responsive}>
+          <div className="m-8  border-2 shadow-lg">
+            {/* sm:m-1 sm:mt-5 */}
+            <Link href="/maruti/xl6">
+              <div className="grid grid-cols-1 bg-gray-200">
+                <div>
+                  <img src={SimilarCars.car1[0]} alt="nnn" />
+                </div>
+                <div className="text-center text-xl font-bold">
+                  {SimilarCars.car1[1]}
+                </div>
+                <div className="flex flex-row">
+                  <div className="text-md font-bold">{SimilarCars.car1[2]}</div>
+                  <div className="text-md text-gray-500 ml-2 mt-0">Onwards</div>
+                </div>
+
+                <div className="text-xs mt-1">Avg. Ex-Showroom price</div>
+              </div>
+            </Link>
+          </div>
+          <div className="m-8  border-2 shadow-lg">
+            <Link href="/maruti/xl6">
+              <div className="grid grid-cols-1 bg-gray-200">
+                <div>
+                  <img src={SimilarCars.car2[0]} alt="nnn" />
+                </div>
+                <div className="text-center text-xl font-bold">
+                  {SimilarCars.car2[1]}
+                </div>
+                <div className="flex flex-row">
+                  <div className="text-md font-bold">{SimilarCars.car2[2]}</div>
+                  <div className="text-md text-gray-500 ml-2 mt-0">Onwards</div>
+                </div>
+
+                <div className="text-xs mt-1">Avg. Ex-Showroom price</div>
+              </div>
+            </Link>
+          </div>
+          {SimilarCars.car3 ? (
+            <div className="m-8  border-2 shadow-lg">
+              <Link href="/maruti/xl6">
+                <div className="grid grid-cols-1 bg-gray-200">
+                  <div>
+                    <img src={SimilarCars.car3[0]} alt="nnn" />
+                  </div>
+                  <div className="text-center text-xl font-bold">
+                    {SimilarCars.car3[1]}
+                  </div>
+                  <div className="flex flex-row">
+                    <div className="text-md font-bold">
+                      {SimilarCars.car3[2]}
+                    </div>
+                    <div className="text-md text-gray-500 ml-2 mt-0">
+                      Onwards
+                    </div>
+                  </div>
+
+                  <div className="text-xs mt-1">Avg. Ex-Showroom price</div>
+                </div>
+              </Link>
+            </div>
+          ) : null}
+
+          {SimilarCars.car4 ? (
+            <div className="m-8 border-2 shadow-lg">
+              <Link href="/maruti/xl6">
+                <div className="grid grid-cols-1 bg-gray-200">
+                  <div>
+                    <img src={SimilarCars.car4[0]} alt="nnn" />
+                  </div>
+                  <div className="text-center text-xl font-bold">
+                    {SimilarCars.car4[1]}
+                  </div>
+                  <div className="flex flex-row">
+                    <div className="text-md font-bold">
+                      {SimilarCars.car4[2]}
+                    </div>
+                    <div className="text-md text-gray-500 ml-2 mt-0">
+                      Onwards
+                    </div>
+                  </div>
+
+                  <div className="text-xs mt-1">Avg. Ex-Showroom price</div>
+                </div>
+              </Link>
+            </div>
+          ) : null}
+        </Carousel>
+      </div>
 
       <MoreCars CompanyName={CompanyName} ThatBrandCars={ThatBrandCars} />
 
