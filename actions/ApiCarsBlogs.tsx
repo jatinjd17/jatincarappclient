1f2017ca8c3b6cc9f46d8b0c82e84c93cba8aebf
jatinjd17 <jatindharmik@gmail.com>
2021-10-08 18:53:35 +0530

    client

--- actions/ApiCarsBlogs.tsx
+++ actions/ApiCarsBlogs.tsx
@@ -3,4 +3,6 @@ export const CarsProduction = "https://carsjatin.herokuapp.com";
 export const BlogsProduction = "https://blogsjatin.herokuapp.com";
 
 // export const ClientProduction = "https://jatincarappclient.vercel.app";
-export const ClientProduction = "https://www.codexjd.xyz";
+// export const ClientProduction = "https://www.codexjd.xyz";
+
+export const ClientProduction = "https://www.indiacarsandbikes.com";
